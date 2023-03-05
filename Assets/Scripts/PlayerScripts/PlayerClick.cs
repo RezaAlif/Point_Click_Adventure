8244e0b097ff4f2f923b4d4fdca25bb63134af85
rezaalif <rezaalif39@gmail.com>
2023-03-05 19:19:08 +0700

    Init

---
+++ Assets/Scripts/PlayerScripts/PlayerClick.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                transform.position = hit.point;
+            }
+        }
+    }
+}
